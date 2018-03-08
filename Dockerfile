154df493b178759fef4990d88b4cb0402fd6a1a2
Jeffrey Wang <jqywang.95@gmail.com>
2018-03-07 17:21:01 -0800

    rewrote my proxy server to work with docker

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM nginx
+COPY client/ /usr/share/nginx/html
+RUN rm /etc/nginx/conf.d/default.conf
+COPY ./default.conf /etc/nginx/conf.d/default.conf
--- server/server.js
+++ server/server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 
-const cors = require('cors');
-
 const app = express();
 
 app.use(express.static(__dirname + '/../client/'));
-app.use(cors());
 app.listen(3000, () => {
   console.log('listening on 3000');
 });
